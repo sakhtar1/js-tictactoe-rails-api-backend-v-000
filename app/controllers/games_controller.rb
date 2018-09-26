499d717493ce1d83f6aa9ebddc3147773bedf154
Sharmina Akhtar <sakht318@gmail.com>
2018-09-26 00:21:09 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,40 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
 
+  def index
+    @games = Game.all
+    respond_to do |f|
+      f.json {render json: @games, status: 200}
+      f.html {render :index}
+    end
+  end
+
+
+  def new
+    @game = Game.new
+  end
+
+
+  def create
+    Game.create(game_params)
+    redirect_to games_path
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    respond_to do |format|
+    format.html { render :show }
+    format.json { render json: @game, status: 200}
+    end
+  end
+
+  def update
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:state)
+  end
+
 end
